31c9da32229a3201348abf5b0f8e2d14eced5037
NoBearRua <52232897+NBeara@users.noreply.github.com>
2025-11-14 10:10:45 +0800

    Add custom packages for ua2f and mwan

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -52,3 +52,9 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-taskplan luci-i18n-taskplan-zh-cn"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
 # 统一文件共享
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES webdav2 luci-app-unishare"
+#校园网
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES ua2f luci-app-ua2f luci-i18n-ua2f-zh-cn"
+#多拨
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES mwan3 luci-app-mwan3 luci-i18n-mwan3-zh-cn"
+
+
